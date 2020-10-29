ba9246a7df5ab55e10fc570f805f7a77ce49dd02
Jacob Doering-Powell <jdoering@lji.org>
2020-10-29 16:44:35 -0400

    updated tests

--- pmbec/tests/pmbec_generator_tests.py
+++ pmbec/tests/pmbec_generator_tests.py
@@ -40,9 +40,49 @@ def test_pm_filter_data():
                     'Position',
                     nrows=49,
                     sep=',')
-    pm.filter_raw_data(raw_data, consolidate=True, positions={2,9}, skip_alleles='2ME')
-    # TODO ADD TO THIS TOMORROW
+    #assert that there are other positions besides 2 and 9 before filtering
+    positions_set = set([])
+    for aa in raw_data:
+        for pos in raw_data[aa]:
+            positions_set.add(pos)
+    assert len(positions_set) > 2
+    assert 1 in positions_set
+    #assert that 2ME is in alleles before filtering
+    twoME_present = False
+    for aa in raw_data:
+        for pos in raw_data[aa]:
+            for allele in raw_data[aa][pos]:
+                if '2ME' in allele:
+                    twoME_present = True
+                    break
+    assert twoME_present
+    raw_data = pm.filter_raw_data(raw_data, 
+                                  consolidate=True, 
+                                  consolidate_on='2ME',
+                                  positions={2,9}, 
+                                  residues_to_consolidate=set(['C']),
+                                  skip_alleles='2ME')
+    #assert that 2ME is not in alleles after filtering
+    twoME_present = False
+    for aa in raw_data:
+        for pos in raw_data[aa]:
+            for allele in raw_data[aa][pos]:
+                if '2ME' in allele:
+                    twoME_present = True
+    assert not twoME_present
+    #assert only posoitions 2 and 9 are in raw data after filtering
+    positions_set = set([])
+    for aa in raw_data:
+        for pos in raw_data[aa]:
+            positions_set.add(pos)
+    assert len(positions_set) == 2
+    for i in range(10):
+        if i != 2 and i != 9:
+            assert i not in positions_set
+        else:
+            assert i in positions_set
     assert pm.filtered == True
+    #assert getting a new raw_data set ressets the filtered flag
     raw_data = pm.get_raw_data("../reduced_cysteine_raw_data/Cysteine_surrogate_raw_data.csv",
                     'Residue',
                     'Position',
@@ -50,3 +90,46 @@ def test_pm_filter_data():
                     sep=',')
     assert pm.filtered == False
     tear_down(pm.job_id)
+
+def test_energy_contribution():
+    pm = setup_pm()
+    exception_thrown = False
+    #assert that exception is thrown properly
+    try:
+        pm.calculate_energy_contribution()
+    except Exception as e:
+        exception_thrown = True
+    assert exception_thrown
+    raw_data = pm.get_raw_data("../reduced_cysteine_raw_data/Cysteine_surrogate_raw_data.csv",
+                    'Residue',
+                    'Position',
+                    nrows=49,
+                    sep=',')
+    # check that if filter is not called before callig calculating energy contribution
+    # then the unfiltered and filtered raw_data csv are the same
+    energy_contribution = pm.calculate_energy_contribution()
+    unfiltered_file = os.getcwd() + '/' + pm.job_id + '/' + pm.job_id + '_unfiltered_raw_data.csv'
+    filtered_file = os.getcwd() + '/' + pm.job_id + '/' + pm.job_id + '_filtered_raw_data.csv'
+    filtered_data = pm.read_intermediate_file(filtered_file)
+    unfiltered_data = pm.read_intermediate_file(unfiltered_file)
+    for filtered_aa, unfiltered_aa in zip(sorted(filtered_data.keys()), sorted(unfiltered_data.keys())):
+        assert filtered_aa == unfiltered_aa
+        for filtered_position, unfiltered_position in zip(sorted(filtered_data[filtered_aa].keys()), sorted(unfiltered_data[unfiltered_aa].keys())):
+            assert filtered_position == unfiltered_position
+            filtered_alleles = sorted(list(filtered_data[filtered_aa][filtered_position].keys()))
+            unfiltered_alleles = sorted(list(unfiltered_data[unfiltered_aa][unfiltered_position].keys()))
+            for filtered_allele, unfiltered_allele in zip(filtered_alleles, unfiltered_alleles):
+                assert filtered_allele == unfiltered_allele
+    filtered_data = pm.filter_raw_data(raw_data, 
+                                  consolidate=True, 
+                                  consolidate_on='2ME',
+                                  positions={2,9}, 
+                                  residues_to_consolidate=set(['C']),
+                                  skip_alleles='2ME')
+    energy_contribution = pm.calculate_energy_contribution()
+    filtered_data = pm.read_intermediate_file(filtered_file)
+    assert len(filtered_data.keys()) != len(unfiltered_data.keys())
+    tear_down(pm.job_id)
+    
+                
+
